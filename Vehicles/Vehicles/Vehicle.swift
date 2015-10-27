dfae5dfedcfdc3aa0861a7cc9ef8fe6cf8cad83d
MFrob <mees.froberg@student.uva.nl>
2015-10-27 05:04:01 -0700

    vehicles toegevoegd

---
+++ Vehicles/Vehicles/Vehicle.swift
@@ -0,0 +1,18 @@
+//
+//  Vehicle.swift
+//  Vehicles
+//
+//  Created by Ray Fix on 9/8/14.
+//  Copyright (c) 2014 Razeware, LLC. All rights reserved.
+//
+
+import Foundation
+
+class Vehicle {
+    var brandName = "null"
+    var modelName = "null"
+    var modelYear = 0
+    var powerSource = "null"
+    var numberOfWheels = 0
+}
+
